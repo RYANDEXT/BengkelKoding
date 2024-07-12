29a2e4526a6caa6d5bd69c88072aebdd47439eb1
RYANDEXT <91421886+RYANDEXT@users.noreply.github.com>
2024-07-12 14:37:46 +0700

    Update Dashboard.py

--- Dashboard.py
+++ Dashboard.py
@@ -6,8 +6,7 @@ import json
 from streamlit_lottie import st_lottie
 from sklearn.preprocessing import MinMaxScaler
 
-
-#judul
+# Judul
 st.write("""
 # Klasifikasi Heart Disease (Gaussian Naive Bayes)
 Aplikasi Berbasis Web Untuk Memprediksi (mengklasifikasi) Penyakit Jantung
@@ -15,40 +14,38 @@ Data yang didapat dari Repositori UCI Machine Learning
 Dataset tersebut adalah dataset penyakit jantung yang terdiri dari 303 data individu
 """)
 
-#image
-
-#lottie animation
+# Lottie animation
 def lottie_file():
     with open("Animation-1704105124757.json", 'r', encoding='utf-8') as p:
         return json.load(p)
-    
+
 path = lottie_file()
 st_lottie(path)
 
-#sidebar
+# Sidebar
 st.sidebar.header('Parameter Inputan')
 
-#input upload file csv untuk parameter inputan, ada inputan filem dan juga inputan manual
-upload_file = st.sidebar.file_uploader("Upload Csv", type= "csv")
+# Input upload file CSV untuk parameter inputan, ada inputan filem dan juga inputan manual
+upload_file = st.sidebar.file_uploader("Upload CSV", type="csv")
 
 if upload_file is not None:
     inputan = pd.read_csv(upload_file)
 else:
     def input_user():
-        age = st.sidebar.slider('Age', 0,150,75)
+        age = st.sidebar.slider('Age', 0, 150, 75)
         sex = st.sidebar.selectbox('Sex', ('Female', 'Male'))
         cp = st.sidebar.selectbox('CP', ('typical angina', 'atypical angina', 'non-anginal pain', 'asymptomatic'))
-        trestbps = st.number_input('Trestbps', 0, 250, 150)
-        chol = st.sidebar.slider('Chol', 0,400,100)
+        trestbps = st.sidebar.slider('Trestbps', 0, 250, 0)
+        chol = st.sidebar.slider('Chol', 0, 400, 100)
         fbs = st.sidebar.selectbox('FBS', ('No', 'Yes'))
-        restecg = st.sidebar.selectbox('Restecg', ('Normal', 'Having ST-T wave abnormality', 'showing probable or definite left venctrivular hypertrophy by Estes "criteria"'))
+        restecg = st.sidebar.selectbox('Restecg', ('Normal', 'Having ST-T wave abnormality', 'showing probable or definite left ventricular hypertrophy by Estes "criteria"'))
         thalach = st.sidebar.slider('Thalach', 0, 400, 150)
-        exang = st.sidebar.selectbox('Exang', ('No','Yes'))
+        exang = st.sidebar.selectbox('Exang', ('No', 'Yes'))
         oldpeak = st.sidebar.slider('Oldpeak', 0.0, 10.0, 5.0)
 
         if sex == 'Female':
             sex = 0
-        elif sex == 'Male':
+        else:
             sex = 1
 
         if cp == 'typical angina':
@@ -57,26 +54,28 @@ else:
             cp = 2
         elif cp == 'non-anginal pain':
             cp = 3
-        elif cp == 'asymptomatic':
+        else:
             cp = 4
 
         if fbs == 'No':
             fbs = 0
-        elif fbs == 'Yes':
+        else:
             fbs = 1
-        
+
         if restecg == 'Normal':
             restecg = 0
         elif restecg == 'Having ST-T wave abnormality':
             restecg = 1
-        else: restecg = 2
+        else:
+            restecg = 2
 
         if exang == 'No':
             exang = 0
-        else: exang = 1
+        else:
+            exang = 1
 
         data = {
-            'age':age,
+            'age': age,
             'sex': sex,
             'cp': cp,
             'trestbps': trestbps,
@@ -90,34 +89,38 @@ else:
 
         fitur = pd.DataFrame(data, index=[0])
         return fitur
+
     inputan = input_user()
 
-
-
-# import dataset
+# Import dataset
 HeartDisease_raw = pd.read_csv('dfClean_hungarian.csv')
 
-#memisahkan dataset atribut dan label
+# Memisahkan dataset atribut dan label
 HeartDisease = HeartDisease_raw.drop(columns=['target'])
-#menggabungkan inputan user kedalam dataset yang sudah dipisah labelnya
+# Menggabungkan inputan user ke dalam dataset yang sudah dipisah labelnya
 df = pd.concat([inputan, HeartDisease], axis=0)
 
-frow = df[:1]
+# Load MinMaxScaler used during training
+scaler = MinMaxScaler()
+scaler.fit(HeartDisease)  # Fit the scaler on the original dataset without the target
 
-# menampilkan parameter hasil inputan
+# Apply normalization to the input data
+df_scaled = scaler.transform(df)
+frow = df_scaled[:1]
+
+# Menampilkan parameter hasil inputan
 st.subheader('Parameter Input')
 
 if upload_file is not None:
-    st.write(df)
+    st.write(inputan)
 else:
-    st.write('Masukkan file csv')
-    st.write(df)
+    st.write('Masukkan file CSV')
+    st.write(inputan)
 
-# load model Gaussian Naive Bayes Classifier
+# Load model Gaussian Naive Bayes Classifier
 load_model = pickle.load(open('modelKNN_HeartDisease.pkl', 'rb'))
 
-# terapkan KNN
-
+# Terapkan model
 prediksi = load_model.predict(frow)
 prediksi_proba = load_model.predict_proba(frow)
 
